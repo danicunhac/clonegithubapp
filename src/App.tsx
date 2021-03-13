453e301f8132e89bd90689755c5e3b84c7109c70
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-13 06:43:11 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { StatusBar, Text } from 'react-native';
+import { Image, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppView } from './styles/app';
+import Icon from 'react-native-vector-icons/Feather';
+import profileImg from '../assets/profile.png';
+
+import { AppView, Header, Container } from './styles/app';
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#161b21" />
       <AppView>
-        <Text>Github Profile</Text>
+        <Header>
+          <Icon
+            name="settings"
+            size={20}
+            color={'#1a73e8'}
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{ marginRight: 16 }}
+          />
+          <Icon name="share" size={20} color={'#1a73e8'} />
+        </Header>
+        <Container>
+          <Image src={profileImg} />
+        </Container>
       </AppView>
     </NavigationContainer>
   );
--- src/styles/app.ts
+++ src/styles/app.ts
@@ -3,5 +3,17 @@ import { View } from 'react-native';
 
 export const AppView = styled(View)`
   flex: 1;
-  background: #0c1117;
+  background: #161b21;
 `;
+
+export const Header = styled(View)`
+  height: 56px;
+  border-bottom-width: 0.5px;
+  border-bottom-color: #f0f6fc;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  padding: 16px;
+`;
+
+export const Container = styled(View)``;
