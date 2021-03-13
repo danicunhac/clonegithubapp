38e87002f02daed6d7b7208e97051139da582326
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-13 08:47:33 -0300

    feat: profile details

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,112 @@
+/* eslint-disable react-native/no-inline-styles */
 import 'react-native-gesture-handler';
 
-import React from 'react';
-import { Image, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StatusBar, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Feather';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+import OctIcon from 'react-native-vector-icons/Octicons';
 import profileImg from '../assets/profile.png';
 
-import { AppView, Header, Container } from './styles/app';
+import {
+  AppView,
+  Header,
+  Container,
+  ProfilePicture,
+  UserName,
+  UserNickname,
+  Status,
+  Description,
+  ProfileDescription,
+  ReadMe,
+} from './styles/app';
+
+interface UserData {
+  login: string;
+  name: string;
+  company: string;
+  location: string;
+  avatar_url: string;
+  bio: string;
+  blog: string;
+  followers: number;
+  following: number;
+}
 
 const App = () => {
+  const [user, setUser] = useState({} as UserData);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/danicunhac')
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#161b21" />
       <AppView>
         <Header>
-          <Icon
+          <FeatherIcon
             name="settings"
             size={20}
             color={'#1a73e8'}
-            // eslint-disable-next-line react-native/no-inline-styles
             style={{ marginRight: 16 }}
           />
-          <Icon name="share" size={20} color={'#1a73e8'} />
+          <FeatherIcon name="share" size={20} color={'#1a73e8'} />
         </Header>
         <Container>
-          <Image src={profileImg} />
+          <ProfilePicture source={profileImg} />
+          <View>
+            <UserName>{user.name}</UserName>
+            <UserNickname>{user.login}</UserNickname>
+          </View>
         </Container>
+        <Container>
+          <Status>🧪 Always coding 🚀🔥</Status>
+        </Container>
+        <ProfileDescription>
+          <Description>{user.bio}</Description>
+          <Description>
+            <FAIcon name="building-o" size={20} color={'#8b949e'} />{' '}
+            {user.company}
+          </Description>
+          <Description style={{ color: '#8b949e' }}>
+            <OctIcon name="location" size={20} color={'#8b949e'} />{' '}
+            {user.location}
+          </Description>
+          <Description>
+            <FeatherIcon name="link" size={20} color={'#8b949e'} /> {user.blog}
+          </Description>
+          <Description>
+            <FeatherIcon name="mail" size={20} color={'#8b949e'} />{' '}
+            danielcc.jp@gmail.com
+          </Description>
+          <Description>
+            <FeatherIcon name="user" size={20} color={'#8b949e'} />{' '}
+            {user.followers} <Text style={{ color: '#8b949e' }}>followers</Text>{' '}
+            {user.following} <Text style={{ color: '#8b949e' }}>following</Text>
+          </Description>
+        </ProfileDescription>
+        <View style={{ height: 16, backgroundColor: '#0f0f0f' }} />
+        <ReadMe>
+          <Text
+            style={{
+              color: '#fff',
+              padding: 16,
+              borderBottomColor: '#8b949e',
+              borderBottomWidth: 0.5,
+              display: 'flex',
+              alignItems: 'center',
+            }}>
+            <FeatherIcon name="smile" size={20} color={'#8b949e'} />{' '}
+            {user.login}/README<Text style={{ color: '#8b949e' }}>.md</Text>
+          </Text>
+        </ReadMe>
       </AppView>
     </NavigationContainer>
   );
--- src/styles/app.ts
+++ src/styles/app.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 export const AppView = styled(View)`
   flex: 1;
@@ -9,11 +9,62 @@ export const AppView = styled(View)`
 export const Header = styled(View)`
   height: 56px;
   border-bottom-width: 0.5px;
-  border-bottom-color: #f0f6fc;
+  border-bottom-color: #8b949e;
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   padding: 16px;
+  margin-bottom: 16px;
 `;
 
-export const Container = styled(View)``;
+export const Container = styled(View)`
+  padding: 0 16px 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProfilePicture = styled(Image)`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  margin-right: 16px;
+`;
+
+export const UserName = styled(Text)`
+  font-weight: bold;
+  font-size: 20px;
+  color: #fff;
+`;
+
+export const UserNickname = styled(Text)`
+  font-size: 16px;
+  color: #8b949e;
+`;
+
+export const Status = styled(Text)`
+  color: rgba(240, 240, 0, 0.95);
+  font-size: 16px;
+  background-color: rgba(170, 150, 0, 0.3);
+  padding: 8px;
+  border-radius: 4px;
+  flex: 1;
+`;
+
+export const Description = styled(Text)`
+  color: #fff;
+  display: flex;
+  font-size: 16px;
+  padding: 0 16px 16px;
+`;
+
+export const ProfileDescription = styled(View)`
+  border-bottom-width: 0.5px;
+  border-bottom-color: #8b949e;
+`;
+
+export const ReadMe = styled(View)`
+  border-top-width: 0.5px;
+  border-top-color: #8b949e;
+  padding: 0 16px;
+`;
